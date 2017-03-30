c0b54c2af40a6de4630a3e9c02006b0f3fd35b94
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2017-03-30 16:28:51 +0530

    Mongoose Promise
    Mongoose.promise causing app to crash because of deprecation

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,7 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
+    mongoose.Promise = global.Promise;
     var db = mongoose.connect(config.db);
     console.log('database running at ' + config.db);
     require('../app/models/tag.server.model');
